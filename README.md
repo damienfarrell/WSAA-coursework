af8f9b578e366bc94e2104ad59a4f62bc5de4c17
damienfarrell <damien.farrell1@gmail.com>
2024-03-17 17:38:42 +0000

    Assignment 04 Complete

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # WSAA-coursework
 ATU HDip in Data Analytics - Web Services and Applications
+
+
+Assignment 01: Current Weather
+
+Assignment 02: CSO
+
+Assignment 03: Github
--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -1,13 +1,20 @@
 from github import Github
 from config import apikey as cfg
-import json
 
+old_name = "Andrew"
+new_name = "Damien"
 g = Github(cfg["apikey"])
+
 repo = g.get_repo("damienfarrell/wsaa-coursework")
+file_path = "assignments/name.txt"
+contents = repo.get_contents(file_path)
+file_content = contents.decoded_content.decode("utf-8")
 
-contents = repo.get_contents("assignments/")
+def replace_name(file_content):
+    updated_content = file_content.replace(old_name, new_name)
+    return updated_content
 
-file_info = []
+updated_content = replace_name(file_content)
 
-with open("repo_contents.json", "w") as json_file:
-    json.dump(file_info, json_file, indent=4)
+# Update the file in the repository
+repo.update_file(file_path, f"Assignment 04: Updated name from {old_name} to {new_name}", updated_content, contents.sha)
